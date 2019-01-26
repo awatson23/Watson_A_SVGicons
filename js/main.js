a49662f369db2ad41d20b2c2a6afdfd3750e6acc
unknown <awatson23>
2019-01-26 18:44:57 -0500

    pushing more javascript work

--- js/main.js
+++ js/main.js
@@ -6,6 +6,84 @@ function request() {
     window.location = 'techicondownload.zip';
 }
 
+let icon1 = document.querySelector('#icon1');
+
+ function logMe() {
+      console.log(this.id);
+    }
+
+icon1.addEventListener("click", logMe)
+
+let icon2 = document.querySelector('#icon2');
+
+ function logMe() {
+      console.log(this.id);
+    }
+
+icon2.addEventListener("click", logMe)
+
+
+let icon3 = document.querySelector('#icon3');
+
+ function logMe() {
+      console.log(this.id);
+    }
+
+icon3.addEventListener("click", logMe)
+
+let icon4 = document.querySelector('#icon4');
+
+ function logMe() {
+      console.log(this.id);
+    }
+
+icon4.addEventListener("click", logMe)
+
+let icon5 = document.querySelector('#icon5');
+
+ function logMe() {
+      console.log(this.id);
+    }
+
+icon5.addEventListener("click", logMe)
+
+
+let icon6 = document.querySelector('#icon6');
+
+ function logMe() {
+      console.log(this.id);
+    }
+
+icon6.addEventListener("click", logMe)
+
+let icon7 = document.querySelector('#icon7');
+
+ function logMe() {
+      console.log(this.id);
+    }
+
+icon7.addEventListener("click", logMe)
+
+let icon8 = document.querySelector('#icon8');
+
+ function logMe() {
+      console.log(this.id);
+    }
+
+icon8.addEventListener("click", logMe)
+
+let icon9 = document.querySelector('#icon9');
+
+ function logMe() {
+      console.log(this.id);
+    }
+
+icon9.addEventListener("click", logMe)
+
+
+
+
+
 
 
 
